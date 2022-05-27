141dbe948f0e7c5b95eb0437edf27877f9d568f4
81CuongVn <89056222+81CuongVn@users.noreply.github.com>
2022-05-27 18:36:43 +0700

    done

---
+++ src/lienhe.php
@@ -0,0 +1,39 @@
+
+
+
+<section class="dv" id="dv">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-12">
+        <h1 class="title text-center">LIÊN HỆ</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-12">
+        
+        <div class="col-md-4">
+            <div class="fitimg">
+              <img src="images/service2.jpg" />
+            </div>
+        </div>
+        <div class="col-md-8">
+            <h2 class="subtitle">Mọi chi tiết liên hệ <a href="https://greenlines-dp.com/">greenlines.com</a> qua những cách sau:</h2>
+
+          <ul class="list">
+          
+            <li style="color:blue"><a href="mailto:alan45walker12@gmail.com"><i><strong>Email: alan45walker12@gmail.com</strong></i></a></li>
+            <li style="color:blue"><i>Hotline: 0365.629.897</i></li>
+            
+        </ul>
+        
+
+        </div>
+		
+      </div>
+    </div>
+  </div>
+
+
+
+</section>
+
---
+++ src/trangchu.php
@@ -0,0 +1,167 @@
+
+<section class="banner">
+  <div id="banner-new" class="owl-carousel">
+    <div class="item" style="background: url(images/phu-quoc-1.jpg) no-repeat center center;">
+      <div class="container">
+		<div class="row">
+		  <div class="col-sm-12 col-md-12 col-lg-12">
+			<div class="banner-box">
+
+             
+			</div>
+		  </div>
+		</div>
+	  </div>
+    </div>
+	<div class="item" style="background: url(images/pq2021.jpg) no-repeat center center;">
+      <div class="container">
+		<div class="row">
+		  <div class="col-sm-12 col-md-12 col-lg-12">
+			<div class="banner-box">
+
+             
+			</div>
+		  </div>
+		</div>
+	  </div>
+    </div>
+	<div class="item" style="background: url(images/slide03.jpg) no-repeat center center;">
+      <div class="container">
+		<div class="row">
+		  <div class="col-sm-12 col-md-12 col-lg-12">
+			<div class="banner-box">
+
+             
+			</div>
+		  </div>
+		</div>
+	  </div>
+    </div>
+    <div class="item" style="background: url(images/slide04.jpg) no-repeat center center;">
+      <div class="container">
+		<div class="row">
+		  <div class="col-sm-12 col-md-12 col-lg-12">
+			<div class="banner-box">
+			 
+			 
+			</div>
+		  </div>
+		</div>
+	  </div>
+    </div>
+
+  </div>  
+</section>
+
+<section class="dv" id="dv">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-12">
+        <h1 class="title text-center"><strong>BÁN VÉ PHÚ QUỐC TÔ THANH CƯỜNG.</strong></h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-12">
+        
+        <div class="col-md-4">
+            <div class="fitimg">
+              <img src="images/service2.jpg" />
+            </div>
+        </div>
+        <div class="col-md-8">
+            <h2 class="subtitle">Vì sao bạn nên mua vé trên <a href="https://greenlines-dp.com/"><strong>greenlines.com</strong></a>?</h2>
+
+          <ul class="list">
+            <li>Tiện lợi, chỉ vài bước đơn giản là có vé ngay.</li>
+            <li>Thanh toán dễ dàng qua ví điện tử MoMo.</li>
+            <li>An toàn và được hỗ trợ tận tình.</li>
+        </ul>
+        <p>Đa số tàu hiện đang vận chuyển hành khách tại Phú Quốc, Côn Đảo, Phú Quý, Lý Sơn là tàu một thân với nhiều hạn chế. Chúng tôi tự tin rằng tàu hai thân của Phú Quốc Express sẽ giúp bạn đặt chân lên đảo với trạng thái tốt nhất.</p>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="container" style="padding-top:50px;">
+	
+	<div class="row">
+          <div class="col-md-12 border-tlr-radius price">
+          
+    
+            
+
+          <div class="panel info-card radius blue-fill shadowDepth1">
+              <h3>Bảng giá Dịch vụ</h3>
+          </div>
+          <div class="panel info-card radius shadowDepth1 msg small-class">
+              <table class="table table-responsive table-bordered price-table">
+                  <thead>
+                      <tr>
+                          <th>Tuyến</th>
+                          <th>Giá Vé Thường</th>
+                          <th>Giá Vé Sang</th>
+                          <th>Giá Vé VIP</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                          <td>Trần Đề ⇔ Côn Đảo</td>
+                          <td>550.000</td>
+                          <td>650.000</td>  
+                          <td>750.000</td>
+                      </tr>
+
+
+                      <tr>
+                          <td>Vũng Tàu ⇔ Côn Đảo</td>
+                          <td>660.000</td>
+                          <td>880.000</td>  
+                          <td>1.200.000</td>
+                      </tr>
+					  
+					   <tr>
+                          <td>Rạch Giá ⇔ Phú Quốc</td>
+                          <td>340.000</td>
+                          <td>Đang cập nhật</td>  
+                          <td>540.000</td>
+                      </tr>
+					   <tr>
+                          <td>Hà Tiên ⇔ Phú Quốc</td>
+                          <td>250.000</td>
+                          <td>Đang cập nhật</td>  
+                          <td>450.000</td>
+                      </tr>
+					   <tr>
+                          <td>Rạch Giá ⇔ Nam Du</td>
+                          <td>250.000</td>
+                          <td>399.000</td>  
+                          <td>550.000</td>
+                      </tr>
+                       <tr>
+                          <td>Rạch Giá ⇔ Hòn Sơn</td>
+                          <td>170.000</td>
+                          <td>299.000</td>  
+                          <td>430.000</td>
+                      </tr>
+                     
+                      
+                  </tbody>
+              </table>
+              <br>
+              <ul class="ul-circle">
+                  <li>Trẻ em dưới 6 tuổi hoặc cao dưới 1m2 được miễn phí vé khi ngồi cùng người lớn. Trẻ em (6 - 11 tuổi) và người cao tuổi (trên 60 tuổi) được áp dụng giảm giá vé Eco.</li>
+              </ul>
+          </div>
+
+
+        </div>
+      </div>
+    
+
+      
+  </div>
+</section>
